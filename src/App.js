c70ffe78373330d9949142713b8466a129922c66
tmpks3 <41902475+tmpks3@users.noreply.github.com>
2019-09-12 23:18:04 +0900

    test
    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+function Food() {
+  return <h1>I love potato</h1>;
+}
 function App() {
-  return <div>Hello!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello!!!!!</h1>
+      <Food fav="kimchi" />
+    </div>
+  );
 }
 
 export default App;
